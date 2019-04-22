c29af86e997839e8b24f0c1fba4282514d31e840
Gleb Skryabin <itsgleb@gmail.com>
2019-04-22 10:01:52 +0100

    Everythings appears to work great

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,22 @@ public class Main {
 
         StartFrame sf = new StartFrame();
         int []theValues = sf.startReturnValues();
+
+//        int []theValues = {  7,0,1,1000,1000,2,2,1,1,0,100,0};
         for(int i=0;i<12;i++){
             System.out.print(theValues[i] + ",");
         }
+        // 7,0,1,100,100,2,2,0,0,0,100,0
+        // 7,0,1,100,100,2,2,0,1,0,100,0
+        // 7,0,1,100,100,2,2,1,0,0,100,0
+        // 7,0,1,100,100,2,2,1,1,0,100,0
         System.out.println();
         System.out.println("started " + LocalDateTime.now());
+
         int redWins, blueWins;
         redWins = blueWins = 0;
         for(int i=0;i<theValues[10];i++){
+            long now = System.currentTimeMillis();
             GameMain gm = new GameMain(theValues[0], theValues[1], theValues[2], theValues);
             if(gm.startGame()==1){
                 System.out.println("Red wins run " + (i+1));
@@ -30,6 +38,7 @@ public class Main {
                 System.out.println("Blue wins run " + (i+1) );
                 blueWins ++;
             }
+            System.out.println("Took " + (System.currentTimeMillis()-now));
         }
         System.out.println("Red " + redWins + ":" + blueWins + " Blue" + "player " + theValues[1] + " " + theValues[2]);
 //
